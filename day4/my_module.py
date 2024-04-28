0ac2de7d9a4e79a29cb7630cb37b2347d463bd58
zeenkeen <zeenalam5@gmail.com>
2024-04-28 22:32:57 +0530

    added the last folder and files

---
+++ day4/my_module.py
@@ -0,0 +1,3 @@
+# this is a moduel and it contains codes/scripts/function/programme that can be used in another progrmme
+my_name = "zeeshan"
+ph_no = 7631099472
---
+++ day4/ran.py
@@ -0,0 +1,12 @@
+import random
+import my_module
+
+random_integer = random.randint(1,100)
+print(random_integer)
+
+print(my_module.my_name)
+print(my_module.ph_no * random_integer)
+print(random_integer)
+
+random_float = random.random()
+print(random_float)
