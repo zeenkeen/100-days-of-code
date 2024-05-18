b429f16146366ec9c2305167b203132cd4ee29e2
zeenkeen <zeenalam5@gmail.com>
2024-05-18 15:53:54 +0530

    small modification

--- day4/treasure_map.py
+++ day4/treasure_map.py
@@ -3,7 +3,8 @@ line2 = ["⬜️","⬜️","️⬜️"]
 line3 = ["⬜️️","⬜️️","⬜️️"]
 map = [line1, line2, line3]
 print("Hiding your treasure! X marks the spot.")
-position = input() # Where do you want to put the treasure?
+position = input("Where do you want to put the treasure? ").capitalize() # Where do you want to put the treasure?
+
 
 # Write your code below this row 👇
 if position == "A1":
